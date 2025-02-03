4d6613a6952069c819f7e2eee586142541106baf
Davidnino21 <davidnino_21@yahoo.com>
2025-02-03 07:25:51 -0800

    started layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,31 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
- 
+
 
   return (
     <>
+      <div className='card'>
+        <h1>XchangeXpert</h1>
+        <div>
+          <input type="number" />
+        </div>
+        <div>
+          <input type="number" />
+        </div>
+        <div>
+        <select>
+            <option value=""></option>
+            <option value=""></option>
+          </select>
+        </div>
+        <div>
+          <select>
+            <option value=""></option>
+            <option value=""></option>
+          </select>
+        </div>
+      </div>
     </>
   )
 }
