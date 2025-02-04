2024166822757cc2a6c6d2bfc872d49813e7db43
Davidnino21 <davidnino_21@yahoo.com>
2025-02-03 20:26:35 -0800

    created layout and added style

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-      <div className='card'>
-        <h1>XchangeXpert</h1>
-        <div>
-          <input type="number" />
-        </div>
-        <div>
-          <input type="number" />
-        </div>
-        <div>
-        <select>
-            <option value=""></option>
-            <option value=""></option>
-          </select>
-        </div>
-        <div>
-          <select>
-            <option value=""></option>
-            <option value=""></option>
-          </select>
+      <div>
+        <div className="card">
+          <h1>XchangeXpert</h1>
+          <div className="amount">
+            <input type="number" />
+            <input type="number" />
+          </div>
+          <div>
+            <h3>=</h3>
+          </div>
+          <div className="symbols">
+            <select>
+              <option value="">EUR</option>
+              <option value="">MXN</option>
+              <option value="">ETH</option>
+              <option value="">USD</option>
+              <option value="">PKR</option>
+              <option value="">CAD</option>
+            </select>
+            <select>
+              <option value="">EUR</option>
+              <option value="">MXN</option>
+              <option value="">ETH</option>
+              <option value="">USD</option>
+              <option value="">PKR</option>
+              <option value="">CAD</option>
+            </select>
+          </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
